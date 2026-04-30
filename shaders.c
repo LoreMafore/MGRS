3f1395c3c9327d0931a56f4708f946a5e425a9a1
Conrad Mercer <144570519+LoreMafore@users.noreply.github.com>
2026-04-29 20:32:31 -0500

    Shit is happening

--- shaders.c
+++ shaders.c
@@ -3,3 +3,62 @@
 //
 
 #include "shaders.h"
+
+
+void orthographic_projection(float origin_x, float width, float height, float origin_y, mat4 projection)
+{
+    uint32_t shader_program = glCreateProgram();
+    glm_ortho(origin_x, width, height, origin_y, -1.0f, 1.0f, projection);
+
+    int projection_location = glGetUniformLocation(shader_program, "projection");
+    glUniformMatrix4fv(projection_location, 1, GL_FALSE, (float*)projection);
+}
+
+char* read_file(const char* filepath)
+{
+    FILE *file = fopen(filepath, "rb");
+
+    if (!file)
+    {
+        fprintf(stderr, "Failed to open shader file: %s\n", filepath);
+        return NULL;
+    }
+
+    fseek(file , 0 , SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char *buffer = malloc(length + 1);
+    fread(buffer, 1, length, file);
+    buffer[length] = '\0';
+    fclose(file);
+    return buffer;
+}
+
+uint32_t compile_shader(const char* filepath, GLenum shader_type)
+{
+    char *source = read_file(filepath);
+
+    if (!source)
+    {
+        fprintf(stderr, "Failed to load shader file: %s\n", filepath);
+        exit(EXIT_FAILURE);
+    }
+
+    uint32_t shader = glCreateShader(shader_type);
+    glShaderSource(shader, 1, (const char **)&source, NULL);
+    glCompileShader(shader);
+    free(source);
+
+    uint32_t success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char info_log[512];
+        glGetShaderInfoLog(shader, 512, NULL, info_log);
+        fprintf(stderr, "Failed to compile shader: %s\n", filepath);
+        exit(EXIT_FAILURE);
+    }
+
+    return shader;
+}
--- shaders.h
+++ shaders.h
@@ -5,6 +5,26 @@
 #ifndef MGRS_SHADERS_H
 #define MGRS_SHADERS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <glad.h>
 #include <cglm/cglm.h>
+#include "Global.h"
+
+typedef struct
+{
+    uint32_t shader_program;
+    uint32_t vao;
+    uint32_t vbo;
+    int vertex_count;
+}grid_data_struct;
+
+char* read_file(const char *filepath);
+uint32_t compile_shader(const char*filepath, GLenum shader_type);
+uint32_t create_shader_program(const char *vertex_shader, const char *fragment_shader);
+void set_orthographic_projection(float origin_x, float width, float height, float origin_y, mat4 projection);
+grid_data_struct build_grid(uint32_t shader_program, float width, float height, float spacing);
+void render_grid(grid_data_struct *grid);
+void destroy_grid(grid_data_struct *grid);
 
 #endif //MGRS_SHADERS_H
